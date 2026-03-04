6bb29fc3fa21dfecc3f482b499b2f421478d666f
Laura <2908538@alu.murciaeduca.es>
2026-03-04 19:21:41 +0100

    Tercera modificación: enlazar Java con XML (Laura)

--- app/src/main/java/com/example/refrimancia/VentanaRecuperarPassword.java
+++ app/src/main/java/com/example/refrimancia/VentanaRecuperarPassword.java
@@ -1,4 +1,30 @@
 package com.example.refrimancia;
 
-public class VentanaRecuperarPassword {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VentanaRecuperarPassword extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.ventana_recuperar_password);
+
+        TextView tvTituloRecuperar = findViewById(R.id.tvTituloRecuperar);
+        TextView tvNuevaPasswordRecuperar = findViewById(R.id.tvNuevaPasswordRecuperar);
+        TextView tvRepetirPasswordRecuperar = findViewById(R.id.tvRepetirPasswordRecuperar);
+        EditText etpNuevaPasswordRecuperar = findViewById(R.id.etpNuevaPasswordRecuperar);
+        EditText etpRepetirPasswordRecuperar = findViewById(R.id.etpRepetirPasswordRecuperar);
+        Button botonActualizarPassword = findViewById(R.id.botonActualizarPassword);
+    }
+
+    //Metodo actualizar contraseña
+    public void onActualizar() {
+
+    }
 }
--- app/src/main/java/com/example/refrimancia/VentanaVerificar.java
+++ app/src/main/java/com/example/refrimancia/VentanaVerificar.java
@@ -1,4 +1,47 @@
 package com.example.refrimancia;
 
-public class VentanaVerificar {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class VentanaVerificar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.ventana_verificar);
+
+        TextView tvTituloVerificar = findViewById(R.id.tvTituloVerificar);
+        TextView tvCorreoVerificar = findViewById(R.id.tvCorreoVerificar);
+        TextView tvSeparadorVerificar = findViewById(R.id.tvSeparadorVerificar);
+        TextView tvEsperarVerificar = findViewById(R.id.tvEsperarVerificar);
+        TextView tvSegundosVerificar = findViewById(R.id.tvSegundosVerificar);
+        TextView tvSegundosTextoVerificar = findViewById(R.id.tvSegundosTextoVerificar);
+        TextView tvCodigoVerificar = findViewById(R.id.tvCodigoVerificar);
+        EditText etCorreoVerificar = findViewById(R.id.etCorreoVerificar);
+        EditText etCodigoVerificar = findViewById(R.id.etCodigoVerificar);
+        Button botonEnviarCode = findViewById(R.id.botonEnviarCode);
+        Button botonVerificar = findViewById(R.id.botonVerificar);
+
+        botonEnviarCode.setOnClickListener(v -> enviarCodigo());
+        botonVerificar.setOnClickListener(v -> verificarCodigo());
+    }
+
+    //Metodo para enviar codigo de validacion
+    public void enviarCodigo() {
+
+    }
+
+    //Metodo para verificar
+    public void verificarCodigo() {
+
+    }
 }

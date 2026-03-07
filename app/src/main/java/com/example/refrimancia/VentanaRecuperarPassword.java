f08790accdbb871cafd3b32479be9a380897b5c8
Laura <2908538@alu.murciaeduca.es>
2026-03-07 19:28:51 +0100

    Cuarta modificacion: primer codigo para funcionalidad en ventanas de recuperar contraseña (Laura)

--- app/src/main/java/com/example/refrimancia/VentanaRecuperarPassword.java
+++ app/src/main/java/com/example/refrimancia/VentanaRecuperarPassword.java
@@ -4,27 +4,42 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class VentanaRecuperarPassword extends AppCompatActivity {
+    TextView tvTituloRecuperar;
+    TextView tvNuevaPasswordRecuperar;
+    TextView tvRepetirPasswordRecuperar;
+    EditText etpNuevaPasswordRecuperar;
+    EditText etpRepetirPasswordRecuperar;
+    Button botonActualizarPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.ventana_recuperar_password);
 
-        TextView tvTituloRecuperar = findViewById(R.id.tvTituloRecuperar);
-        TextView tvNuevaPasswordRecuperar = findViewById(R.id.tvNuevaPasswordRecuperar);
-        TextView tvRepetirPasswordRecuperar = findViewById(R.id.tvRepetirPasswordRecuperar);
-        EditText etpNuevaPasswordRecuperar = findViewById(R.id.etpNuevaPasswordRecuperar);
-        EditText etpRepetirPasswordRecuperar = findViewById(R.id.etpRepetirPasswordRecuperar);
-        Button botonActualizarPassword = findViewById(R.id.botonActualizarPassword);
+        tvTituloRecuperar = findViewById(R.id.tvTituloRecuperar);
+        tvNuevaPasswordRecuperar = findViewById(R.id.tvNuevaPasswordRecuperar);
+        tvRepetirPasswordRecuperar = findViewById(R.id.tvRepetirPasswordRecuperar);
+        etpNuevaPasswordRecuperar = findViewById(R.id.etpNuevaPasswordRecuperar);
+        etpRepetirPasswordRecuperar = findViewById(R.id.etpRepetirPasswordRecuperar);
+        botonActualizarPassword = findViewById(R.id.botonActualizarPassword);
     }
 
     //Metodo actualizar contraseña
     public void onActualizar() {
+        String nuevaPwd = etpNuevaPasswordRecuperar.getText().toString().trim();
+        String repetirPwd = etpRepetirPasswordRecuperar.getText().toString().trim();
 
+        if(nuevaPwd == repetirPwd) {
+            //PARTE DE BASE DE DATOS
+        } else {
+            Toast.makeText(this,"Las contraseñas no coinciden",Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/refrimancia/VentanaRegistro.java
+++ app/src/main/java/com/example/refrimancia/VentanaRegistro.java
@@ -1,4 +1,40 @@
 package com.example.refrimancia;
 
-public class VentanaRegistro {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VentanaRegistro extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.ventana_registro);
+
+        TextView tvTituloRegistro = findViewById(R.id.tvTituloRegistro);
+        TextView tvNombreUserRegistro = findViewById(R.id.tvNombreUserRegistro);
+        TextView tvAviso1Registro = findViewById(R.id.tvAviso1Registro);
+        TextView tvCorreoRegistro = findViewById(R.id.tvCorreoRegistro);
+        TextView tvAviso2Registro = findViewById(R.id.tvAviso2Registro);
+        TextView tvContrasenaRegistro = findViewById(R.id.tvContrasenaRegistro);
+        TextView tvNombreRegistro = findViewById(R.id.tvNombreRegistro);
+        TextView tvApellidosRegistro = findViewById(R.id.tvApellidosRegistro);
+        TextView tvFechaRegistro = findViewById(R.id.tvFechaRegistro);
+        EditText etNombreUserRegistro = findViewById(R.id.etNombreUserRegistro);
+        EditText etCorreoRegistro = findViewById(R.id.etCorreoRegistro);
+        EditText etpContrasenaRegistro = findViewById(R.id.etpConstrasenaRegistro);
+        EditText etNombreRegistro = findViewById(R.id.etNombreRegistro);
+        EditText etApellidosRegistro = findViewById(R.id.etApellidosRegistro);
+        EditText etdFechaRegistro = findViewById(R.id.etdFechaRegistro);
+        Button botonRegistro = findViewById(R.id.botonRegistro);
+    }
+
+    //Metodo para boton registrar
+    public void registrar() {
+
+    }
 }
